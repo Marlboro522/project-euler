c7036d2a17c40c0082c084845796067da690c034
Wasted_Marl <ilu.raghus@icloud.com>
2024-08-16 17:23:46 -0600

    problem 10

--- 10.py
+++ 10.py
@@ -1,17 +1,31 @@
 #find the sum of all primes less than 20000000
 import math
-sum = 2
-
+import time
+t_sum = 2
+n = 200000
 #Brute force.u
-for i in range(3,2000000,2):
+start=time.time()
+for i in range(3,n,2):
     prime_check=True
     for j in range(2, int(math.sqrt(i)) + 1):
         if(i %j ==0):
             prime_check=False
             break;
     if(prime_check):
-        sum += i
-print(sum)
+        t_sum += i
+print("Brute Force: " + str(t_sum))
+print("Brute Force Time: " + str(time.time()-start))
 
 #sieve of Eratosthenes
-
+def solution_sieve(n):
+    sieve = [True] * (n+1) #Marking all numbers are prime. 
+    p=2 #Initial Prime Number
+    while p*p <=n: #this will be false when pursuing number p is greater than sqrt(n)
+        if sieve[p]:
+            for i in range(p*p, n+1, p):
+                sieve[i] = False
+        p+=1
+    return sum(i for i in range(2,n) if sieve[i])
+start =time.time()
+print("Sieve of Eratosthenes: " + str(solution_sieve(n)))
+print("Sieve of Eratosthenes Time: " + str(time.time()-start))
